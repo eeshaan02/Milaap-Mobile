50cdd436154c5e48cef5a215f007f944d7e6f35c
eeshaan02 <eeshaan@milaap.org>
2024-10-22 14:50:49 +0545

    added svg pkg

--- App.tsx
+++ App.tsx
@@ -28,8 +28,13 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import { G, Line, Text as SvgText } from 'react-native-svg';
+//import { BarChart, XAxis } from 'react-native-svg-charts';
+//import { PieChart } from 'react-native-svg-charts';
+
 import Video, {VideoRef} from 'react-native-video';
 const { width, height } = Dimensions.get('window')
+
 
 const VideoPlayer = () => {
  const videoRef = useRef<VideoRef>(null);
@@ -61,6 +66,75 @@ function Animation() {
   );
 }
 
+//const ShareCampaignComponent = ({ socialShares }: any) => {
+  //const socialShareDonations_Data = [
+    //{
+      //key: 'Whatsapp',
+      //value: socialShares?.whatsapp,
+      //svg: { fill: '#FFC38B' },
+    //},
+    //{
+      //key: 'Facebook',
+      //value: socialShares?.facebook,
+      //svg: { fill: '#FFD7BB' },
+    //},
+    //{
+      //key: 'Instagram',
+      //value: socialShares?.instagram,
+      //svg: { fill: '#FCEEE8' },
+    //},
+    //{
+      //key: 'Mail',
+      //value: socialShares?.mail,
+      //svg: { fill: '#DA8900' },
+    //},
+    //{
+      //key: 'Others',
+      //value: socialShares?.others,
+      //svg: { fill: '#FFB056' },
+    //},
+  //]
+
+  //return (
+    //<View
+      //style={{
+        //height: (height / 640) * 200,
+      //}}
+    //>
+      //{Object.keys(socialShareDonations_Data).length && (
+        //<>
+          //<PieChart
+            //style={{ height: 150, position: 'relative', right: 80 }}
+            //data={socialShareDonations_Data}
+            //outerRadius={'80%'}
+            //innerRadius={10}
+          ///>
+          //<View style={{ position: 'absolute', right: (width / 360) * 50, top: 0 }}>
+            //{socialShareDonations_Data.map((item, index) => {
+              //return (
+                //<View
+                  //style={{ flexDirection: 'row', marginTop: 8 }}
+                  //key={index}
+                //>
+                  //<View
+                    //style={[
+                      //styles.legendColor,
+                      //{ backgroundColor: item.svg.fill },
+                    //]}
+                  ///>
+                  //<Text style={styles.legendText}>{item.key}</Text>
+                //</View>
+              //)
+            //})}
+          //</View>
+        //</>
+      //)}
+    //</View>
+  //)
+//}
+
+
+
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -69,7 +143,7 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <Animation/>
+    <Animation />
   );
 }
 
@@ -99,6 +173,19 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  legendText: {
+    fontSize: 14,
+    color: 'white',
+    fontWeight: '500',
+    lineHeight: 19,
+    fontFamily: 'Avenir-Heavy',
+    paddingLeft: 8,
+  },
+  legendColor: {
+    height: 16,
+    width: 16,
+    borderRadius: 2,
   },
 });
 
